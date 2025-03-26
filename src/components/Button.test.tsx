757a84a63f2d51fce3418f7894566671e45fee90
Prawee Wongsa <prawee@hotmail.com>
2025-03-26 09:47:39 +0700

    prioty load script

--- src/components/Button.test.tsx
+++ src/components/Button.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Button from "./Button";
-import { render } from "@testing-library/react";
 import { test } from "@jest/globals";
+import { render } from "@testing-library/react";
+import Button from "./Button";
 
 test("Testing button component", () => {
     render(<Button />);
